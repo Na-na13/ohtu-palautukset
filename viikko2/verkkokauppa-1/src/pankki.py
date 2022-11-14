a29816285aac52270f2f0b85ef073d112a8f19a7
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2022-11-14 22:05:57 +0200

    verkkokauppa-1 exercises

---
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -0,0 +1,15 @@
+from kirjanpito import kirjanpito as defaul_kirjapito
+
+class Pankki:
+    def __init__(self, kirjanpito=defaul_kirjapito):
+        self._kirjanpito = kirjanpito
+
+    def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
+        self._kirjanpito.lisaa_tapahtuma(
+            f"tilisiirto: tililtä {tililta} tilille {tilille} viite {viitenumero} summa {summa}e"
+        )
+
+        # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return True
+
+pankki = Pankki()
