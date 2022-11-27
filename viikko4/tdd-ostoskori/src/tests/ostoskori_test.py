c5ac57b9c446f3c9bf85f71df9e82e0a01950843
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2022-11-27 23:44:33 +0200

    total amount correct after adding two different products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,18 +5,25 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
-        self.tuote = Tuote("maito", 3)
+        self.maito = Tuote("maito", 3)
+        self.voi = Tuote("voi", 4)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_tavaroiden_maara_oikein_lisaamisen_jalkeen(self):
-        self.kori.lisaa_tuote(self.tuote)
+        self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_ostoskorin_hinta_oikein_yhden_tuotteen_lisayksen_jalkeen(self):
-        self.kori.lisaa_tuote(self.tuote)
+        self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisayt_muuttaa_tavaramaaraa_oikein(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.voi)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
